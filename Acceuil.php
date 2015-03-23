1f4dc3a2465d3a997b177742a917229096b43f57
esimion <evy_971@msn.com>
2015-03-23 10:16:12 +0100

    first commit

---
+++ Acceuil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset= "utf-8"> <!-- Afficher caractères spéciaux -->
+	<link rel="stylesheet" type="text/css" href="Acceuil.css">
+	<title></title>
+</head>
+<?php include("Trame-app.html"); ?>
+
+	<div class="centre">
+	<p>Je ramène ma fraise</p>
+	<p ><img id="leg" src="cropped-fruits-et-legumes-de-saison.png"></p>
+	
+	<table>
+		<tr>
+			<td>
+				<h2>Qui sommes nous ?</h2>
+				<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+		  	bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p></td>
+		  	<td><img class="imgbody" src="marche-legumes-75071.jpg"/></td>
+		</tr>
+	  	<tr>
+	  		<td><img class="imgbody" src="legumes_1.jpg"/></td>
+			<td>
+				<h2>Comment ça marche ?</h2>
+				<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+			bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+		  	bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p></td>
+		</tr>
+  </table>
+</div>
+
+</body>
+</html>
---
+++ projetApp2/BaseSite.php
@@ -0,0 +1,25 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="BaseSite.css">
+
+	<div id="bandelogo">
+		<table>
+			<tr>
+				<td><img src="logo-de-fruit-21373675.jpg"></td>
+				<td><input id="search" type="search" placeholder="Rechercher un produit">
+				<input type="image" src="magnifier13.png" value="search"></td>
+				<td id="compte"><a href="LIEN"><img src="user168.png">Mon Compte</a></td>
+				<td id="panier"><a href="LIEN"><img src="add169.png"><br/>Mon Panier</a></td>
+			</tr>
+		</table>	
+	</div>
+
+	<div id="navbar">
+		<ul>
+			<a href=""><li>Accueil</li></a>
+			<a href=""><li>Profil</li></a>
+			<a href=""><li>Transaction</li></a>
+			<a href=""><li>Forum</li></a>
+			<a href=""><li>FAQ</li></a>
+		</ul>	
+	
+</div>
