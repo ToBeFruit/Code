51f8b8154fb0daab69cb03e0fadcaa6e90da51ea
esimion <evy_971@msn.com>
2015-04-07 13:53:33 +0100

    début fofo

--- SiteEvy/BaseSite.php
+++ SiteEvy/BaseSite.php
@@ -1,27 +1,30 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="BaseSite.css">
 
+
 <!--TODO ♠Navbar en transparence-->
 
 <div id="bandelogo">
 		<table>
 			<tr>
-				<td><img src="logo-de-fruit-21373675.jpg"></td>
+				<td><img src="Images\logo-de-fruit-21373675.jpg"></td>
 				<td><input id="search" type="search" placeholder="Rechercher un produit">
-				<input type="image" src="magnifier13.png" value="search"></td>
-				<td id="compte"><a href="LIEN"><img src="user168.png">Mon Compte</a></td>
-				<td id="panier"><a href="LIEN"><img src="add169.png"><br/>Mon Panier</a></td>
+				<input type="image" src="Images\magnifier13.png" value="search"></td>
+				<td id="compte"><a href="LIEN"><img src="Images\user168.png">Mon Compte</a></td>
+				<td id="panier"><a href="LIEN"><img src="Images\add169.png"><br/>Mon Panier</a></td>
 			</tr>
 		</table>	
 	</div>
 
 	<div id="navbar">
 		<ul>
-			<a href=""><li>Accueil</li></a>
+			<a href="Accueil.php"><li>Accueil</li></a>
 			<a href=""><li>Profil</li></a>
 			<a href=""><li>Transaction</li></a>
-			<a href=""><li>Forum</li></a>
+			<a href="ChatForum.php"><li>Forum</li></a>
 			<a href=""><li>FAQ</li></a>
 		</ul>	
 	
 </div>
+<div id="mybody">
+
